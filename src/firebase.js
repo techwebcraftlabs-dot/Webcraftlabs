c0d762843b5aaf9a6024a7053e53e874d4799e8c
Zonal <zonalrealtydb@gmail.com>
2026-06-07 16:32:04 +0800

    Latest Version

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDT17lAEJtdcDVltf7Wf8xKttBvuXloM5M",
+  authDomain: "zonal-project.firebaseapp.com",
+  projectId: "zonal-project",
+  storageBucket: "zonal-project.firebasestorage.app",
+  messagingSenderId: "524581590483",
+  appId: "1:524581590483:web:cde9d404d0d9df226f8b26",
+  measurementId: "G-N5WE2882WH"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+const analytics = getAnalytics(app);
